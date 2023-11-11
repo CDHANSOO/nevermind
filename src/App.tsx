a6e36c51695b95cd92e25f61c6c1774fee139d52
Jung-eunwoo <wjddmsdn_8976@naver.com>
2023-11-11 17:41:09 +0900

    feature/2 style page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <h1 className='font-bold text-3xl'>안녕</h1>
+    </div>
   )
 }
 
