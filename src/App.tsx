110b9236728767a4b90e85a640be8ca9df301232
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2023-11-13 17:04:02 +0900

    feat/NavigationBar.tsx 추가 및 componenet 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import './App.css';
+import './App.css'
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
+
   return (
     <div>
-      하이하이
+      <NavigationBar/>
+      <Routes>
+        <Route path='/' element={<Home />} >
+        </Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Tpo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tpo = () => {
+  return (
+    <div>Tpo</div>
+  )
+}
+
+export default Tpo
