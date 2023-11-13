f23dfeff5e605971460868392ac9aa530d9bceba
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2023-11-13 17:20:05 +0900

    Style.tsx router 연결 및 기본 레이아웃 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
+import Home from './pages/Home';
+import Content from './pages/Content';
+import Style from './pages/Style';
+import Tpo from './pages/Tpo';
+
 
 function App() {
-
   return (
     <div>
-      <h1 className='font-bold text-3xl'>안녕</h1>
+      <NavigationBar/>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/content' element={<Content />} />
+          <Route path='/tpo' element={<Tpo />} />
+          <Route path='/style' element={<Style />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Style.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Style = () => {
+    return (
+        <div className='px-20'>
+            <div>
+                <div>
+                    <h2>NEVERMIND</h2>
+                    <p>트렌드를 따라가는 것, 멋있는 일이지만 동시에 번거로운 일이죠.<br />
+                        놀랍도록 간단하게 실시간 인기 있는 패션을을 살펴보세요</p>
+                </div>
+                <div className=''>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Style
