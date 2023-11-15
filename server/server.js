e5f5487efcc4d66d4ceb936b5ed9afb293588da6
GitHansu <138184682+GitHansu@users.noreply.github.com>
2023-11-15 15:36:17 +0900

    server 세팅중
    server 세팅중

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const dotenv = require('dotenv')
+
+dotenv.config()
+const app = express()
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'))
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(session({
+    resave : false,
+    saveUninitialized : false ,
+    secret : process.env.COOKIE_SECRET,
+    cookie :{
+        httpOnly : true, 
+        secure : false,
+    },
+    name : 'session-cookie'
+}))
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express');
+});
+
+app.use((req, res, next)=>{
+    console.log('모든 요청에 다 실행됩니다');
+    next();
+})
+
+// 정적 파일 서빙
+app.use(express.static(path.join(__dirname, '../src/App.tsx')));
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기중');
+});
+
