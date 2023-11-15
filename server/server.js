039c0d0c2c95c79c637f13a82c13f32926885165
GitHansu <138184682+GitHansu@users.noreply.github.com>
2023-11-15 23:05:27 +0900

    server setting
    server setting

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const dotenv = require('dotenv');
+const multer = require('multer')
 
 dotenv.config()
 const app = express()
@@ -12,14 +13,24 @@ const app = express()
 app.set('port', process.env.PORT || 3000);
 
 // 공통 미들웨어
-app.use(morgan('dev'))
-app.use('/', express.static(path.join(__dirname, 'public')))
+
+// 클라이언트에 온 요청, 응답을 서버에 기록해주는 미들웨어
+app.use(morgan('dev')) // 개발시엔 dev, 배포시엔 combined
+
+// 정적인 파일들을 제공하는 라우터 (public)folder에 넣고 정적 파일들을 불러오자! 요청경로 ,실제경로
+app.use('/', express.static(path.join(__dirname, 'public'))) // 끝
+
+// 요청에 동봉된 쿠키를 req.cookies 객체로 만들어줌 
+// 생성 res.cookie(키, 값, 옵션) , 제거 res.clearCookie
+app.use(cookieParser('process.env.COOKIE_SECRET'))
+// json 읽어오기.
 app.use(express.json())
-app.use(express.urlencoded({extended : false}))
-app.use(cookieParser(process.env.COOKIE_SECRET))
+// form submit할때 parsing해줌 
+app.use(express.urlencoded({extended : true}))
+// session 설정
 app.use(session({
-    resave : false,
-    saveUninitialized : false ,
+    resave : false, // 세션에 수정사항이 없어도 저장할거야?
+    saveUninitialized : false , // 세션에 저장할 내역이 없어도 생성할거야?
     secret : 'process.env.COOKIE_SECRET',
     cookie :{
         httpOnly : true, 
@@ -27,6 +38,80 @@ app.use(session({
     },
     name : 'session-cookie'
 }))
+
+/** 미들웨어 확장할 때
+    app.use('/', (req, res, next) => {
+        로그인시에만 정적인 파일 보여주기! 
+        if(req.session.id){
+            express.static(__dirname , 'public')(req,res,next)
+        }
+        else{
+            next();
+        }
+    })
+ */
+
+
+app.use((req, res, next)=>{
+    console.log('요청에 실행하고 싶어요');
+    next();
+}, (req, res, next)=>{
+    try{
+        
+        console.log('nevermind');
+        next()
+    } catch(error){
+        // error 처리 미들웨어로 error 옮겨주기
+        next(error);
+    }
+})
+
+const upload = multer({
+    storage : multer.diskStorage({
+        destination(req, file, done){
+            done(null, 'uploads/')
+        } , 
+        filename(req, file, done){
+            const ext = path.extname(file.originalname)
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext)
+        },
+    }),
+    limits : {fileSize : 5 * 1024 * 1024}
+})
+
+// 미들웨어간 data를 전송하고 싶을 땐 => res.locals.data
+
+// 라우터들
+// 주의점 : 한 라우터에 res.send or res.json같은게 2번이상 나와선 안된다. 한번씩만!!!
+app.get('/', (req, res) => {
+
+    // 개인의 저장공간
+    req.session.id = 'hello'
+    res.json({ hello : 'Hansu'});
+ 
+});
+
+app.get('/upload', (req,res)=>{
+    res.sendFile(path.join(__dirname, './multipart.html'))
+})
+
+app.get('/abc', (req, res) => {
+    res.send('Hello, Express');
+
+    // res.writeHead(200 , {}) 응답하고 writeHead 해도 오류!!!
+
+});
+
+app.get('/', (req, res) => {
+
+    res.json({ hello : 'Hansu'});
+    // res.json은 return이 아니다 => 아래 콘솔도 실행됨!!
+    console.log('hello hansu');
+});
+
+
+
+
 
 // 404 처리 미들 웨어 
 app.use((req, res, next)=>{
@@ -37,16 +122,9 @@ app.use((req, res, next)=>{
 
 // error 미들 웨어 
 app.use((err, req, res, next)=>{
-    console.log('모든 요청에 다 실행됩니다');
-    next();
+    console.error(err);
+    res.send('error가 발생했다!!!!!')
 })
-
-
-// 라우터들
-app.get('/abc', (req, res) => {
-    res.send('Hello, Express');
-
-});
 
 
 
