8d3ee704b296cd6b05ec9f28df6d728c3f1a0299
GitHansu <138184682+GitHansu@users.noreply.github.com>
2023-11-15 17:46:56 +0900

    미들웨어 셋힝
    middle ware?

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const path = require('path');
-const morgan = require('morgan')
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const dotenv = require('dotenv')
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
 
 dotenv.config()
 const app = express()
 
+// process.env => 현재 port 아니면 3000
 app.set('port', process.env.PORT || 3000);
 
+// 공통 미들웨어
 app.use(morgan('dev'))
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use(express.json())
@@ -18,7 +20,7 @@ app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(session({
     resave : false,
     saveUninitialized : false ,
-    secret : process.env.COOKIE_SECRET,
+    secret : 'process.env.COOKIE_SECRET',
     cookie :{
         httpOnly : true, 
         secure : false,
@@ -26,17 +28,28 @@ app.use(session({
     name : 'session-cookie'
 }))
 
-app.get('/', (req, res) => {
-    res.send('Hello, Express');
-});
-
+// 404 처리 미들 웨어 
 app.use((req, res, next)=>{
+    res.send('404 지롱~!')
+    next();
+})
+
+
+// error 미들 웨어 
+app.use((err, req, res, next)=>{
     console.log('모든 요청에 다 실행됩니다');
     next();
 })
 
-// 정적 파일 서빙
-app.use(express.static(path.join(__dirname, '../src/App.tsx')));
+
+// 라우터들
+app.get('/abc', (req, res) => {
+    res.send('Hello, Express');
+
+});
+
+
+
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기중');
