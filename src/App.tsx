f4514425017d16cb9bca1377c5880a3675732cbf
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2023-11-17 15:07:29 +0900

    merge main

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
@@ -6,19 +6,19 @@ import Home from './pages/Home';
 import Content from './pages/Content';
 import Style from './pages/Style';
 
-import Tpo from '@/pages/Tpo';
-import Footer from '@components/footer';
-import Detail from '@/pages/Detail';
-import Sign from '@/pages/Sign';
+import Tpo from './pages/Tpo';
+import Footer from './pages/Footer';
+import Detail from './pages/Detail';
+import Sign from './pages/Sign';
 import SignUp from './components/SignUp';
 import ContentDetail from 'pages/ContentDetail';
 
 
 const App: React.FC = () => {
-    const [userInput, setUserInput] = useState('');
+  const [userInput, setUserInput] = useState('');
   return (
     <div>
-      <NavigationBar/>
+      <NavigationBar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/content' element={<Content />} />
