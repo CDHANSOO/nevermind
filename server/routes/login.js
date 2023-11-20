ccf2f51b8f5d331d83761e7fda22f5a094b21d43
GitHansu <138184682+GitHansu@users.noreply.github.com>
2023-11-20 15:09:32 +0900

    login  로직 1차로 보내기
    이걸로도 어느정도 해결은 가능하지만 비밀번호 해쉬나 세션 같은 보완해야 할 점들이 있음.

--- server/routes/login.js
+++ server/routes/login.js
@@ -9,25 +9,37 @@ const path = require('path');
  * querystring = req.query
  */
 
-async function main() {
+async function login(req, res) {
   try {
     const connection = await dbConnector.dbConnect();
+    let { email, password } = req.body;
+    // 데이터베이스 작업 수행...
+    // 로그인 로직
+    // DB에 아이디가 존재하는지 확인
+    const result = await connection.execute(`SELECT * FROM T_USER WHERE USER_ID = :email and USER_PW = :password`, [email, password]);
 
-    // 여기서 데이터베이스 작업 수행
-    // 간단한 SELECT 쿼리 실행
-    const result = await connection.execute('SELECT * FROM T_CLOTHE');
-
-    // 쿼리 결과 출력
     console.log('쿼리 결과:', result.rows[0]);
+    console.log(req.body);
 
+    // 회원가입 로직
+    if (result.rows.length === 0) {
+      console.log('if 문 실행');
+      res.json({ login: false });
+    } else {
+      // 로그인 성공
+      console.log('로그인 성공');
+      res.json({ login: true });
+    }
+  } catch (err) {
     // 작업이 끝난 후 연결을 종료
+
+    console.error('오류 발생:', err.message);
+    res.status(500).send('서버 오류');
+  } finally {
     await connection.close();
     console.log('DB 연결 종료 성공!');
-  } catch (err) {
-    console.error('오류 발생:', err.message);
   }
 }
-
 // get / 라우터
 // 라우터들
 // 주의점 : 한 라우터에 res.send or res.json같은게 2번이상 나와선 안된다. 한번씩만!!!
