f0479238df447ae61ccf3574d6c7f2ceb8743e51
GitHansu <138184682+GitHansu@users.noreply.github.com>
2023-11-20 17:38:15 +0900

    login, join 페이지 백엔드 기능 구현완료
    로그인 회원가입 페이지 백엔드 기능은 구현 완료했고 ,암호를 hashed한 채로 데이터베이스에 저장해 보안에 대한 안정성을 높였습니다.

--- server/routes/join.js
+++ server/routes/join.js
@@ -3,16 +3,24 @@ const multer = require('multer');
 const router = express.Router();
 const dbConnector = require('../config/dbConnector');
 const path = require('path');
+const bcrypt = require('bcrypt');
 /**
  * 라우트 매개변수
  * :id를 넣으면 req.params.id로 받을 수 있음
  * querystring = req.query
  */
 
+// 회원가입 함수
 async function join(req, res) {
   try {
     const connection = await dbConnector.dbConnect();
     let { email, password, name, nick, birth, gender, passwordCheck } = req.body;
+    // 비밀번호 체크
+    if (password !== passwordCheck) {
+      console.log('비밀번호가 일치하지 않습니다');
+      res.json({ checkPassword: false });
+      return;
+    }
     // 데이터베이스 작업 수행...
     // 회원가입 로직...
     // DB에 아이디가 이미 존재하는지 확인
@@ -25,23 +33,24 @@ async function join(req, res) {
     if (result.rows.length > 0) {
       console.log('if 문 실행');
       res.json({ id_duplication: true });
-    } else if (password !== passwordCheck) {
-      console.log('else if 문 실행');
-      res.send('비밀번호 달라');
     } else {
       console.log('else문 실행');
+      // 비밀번호 해시 생성
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
       // 회원가입 로직 (바인드 변수 사용)
       await connection.execute(
         `INSERT INTO T_USER (
             USER_ID, USER_PW, USER_SALT, USER_NAME, USER_NICK, USER_GENDER, USER_BIRTH, USER_JOINDATE
            ) VALUES (
-            :email, :password, :passwordCheck, :name, :nick, :gender, TO_DATE(:birth, 'YYYY-MM-DD'),  SYSDATE
+            :email, :hashedPassword, :hashedPassword, :name, :nick, :gender, TO_DATE(:birth, 'YYYY-MM-DD'),  SYSDATE
            ) 
            `,
-        [email, password, passwordCheck, name, nick, gender, birth],
+        [email, hashedPassword, hashedPassword, name, nick, gender, birth],
         { autoCommit: true },
       );
       console.log('회원가입 성공');
+      res.json({ registered: true }); // 회원가입 성공!
     }
 
     // 작업이 끝난 후 연결을 종료
