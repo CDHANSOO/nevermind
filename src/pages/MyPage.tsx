04491202ec413b30f37bbb1b09553f6235c284c9
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2023-11-22 09:24:04 +0900

    fix mypage image import 효율성 개선 시도..

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,32 +1,52 @@
-// import LikedStyleSection from '@components/myPage/LikedStyleSection'
-import MyPageCarousel from '@components/myPage/MyPageCarousel'
+import LikedStyleSection from '@components/myPage/LikedStyleSection'
+// import MyPageCarousel from '@components/myPage/MyPageCarousel'
 import MyPageTop from '@components/myPage/MyPageTop'
 import React from 'react'
 
 
-// 231121 : 정은우
-// 나중에 이미지 데이터 받아서 연동할 것 일단은 레이아웃 구성을 위해 주석 처리
-// interface  likedOufits{
-//   codyImg:codyImgProps,
-
-// }
 
 const MyPage: React.FC = () => {
-  const DibsH2TextString:string[]=[
-    '내가 찜한 코디',
-    '내가 찜한 옷'
-  ]
+  // const DibsH2TextString: string[] = ['내가 찜한 코디', '내가 찜한 옷']
+
+  //슬라이드 설정
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 7,
+    slidesToScroll: 5,
+  };
+  const likedOutfits: {
+    category: string;
+    date: string;
+}[] = [
+    //임시적
+    { category: '댄디·캐주얼', date: '2023.10.28' },
+    { category: '댄디·캐주얼', date: '2023.10.28' },
+    { category: '댄디·캐주얼', date: '2023.10.28' },
+    { category: '댄디·캐주얼', date: '2023.10.28' },
+    { category: '댄디·캐주얼', date: '2023.10.28' },
+    { category: '댄디·캐주얼', date: '2023.10.28' },
+    { category: '댄디·캐주얼', date: '2023.10.28' },
+    { category: '댄디·캐주얼', date: '2023.10.28' },
+    { category: '댄디·캐주얼', date: '2023.10.28' },
+    { category: '댄디·캐주얼', date: '2023.10.28' },
+    { category: '댄디·캐주얼', date: '2023.10.28' },
+    { category: '댄디·캐주얼', date: '2023.10.28' },
+    { category: '댄디·캐주얼', date: '2023.10.28' },
+  ];
+
   return (
     <div className='w-full h-auto relative top-[70px] bg-gray-200 flex justify-center'>
       <div className='w-3/5 bg-gray-300'>
         <MyPageTop />
         <div className='border-t-2'>
-          {DibsH2TextString.map((string, index) => (
+          {/* {DibsH2TextString.map((string, index) => (
             <div className='mt-28' key={index}>
-              <MyPageCarousel h2Text={DibsH2TextString} />
+              <MyPageCarousel h2Text={string} />
             </div>
-          ))}
-          {/* <LikedStyleSection /> */}
+          ))} */}
+          <LikedStyleSection />
           <div className='mt-24'>
             <h2 className='text-2xl font-extrabold mb-4'>무슨 옷을 많이 입었을까?</h2>
           </div>
