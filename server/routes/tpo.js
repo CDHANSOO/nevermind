da213260ac07041d80b69795eaa90ec463e39a5e
GitHansu <138184682+GitHansu@users.noreply.github.com>
2023-11-23 15:49:37 +0900

    tpo 백엔드 수정
    tpo 백엔드 기능 필터링시 랜덤한 값 뜨도록 변경
     구현내용2
    style_ids에는 있지만 clothe_id는 아닌 경우 발생하는 오류 수정 이 2가지 오류를 수정했다.

--- server/routes/tpo.js
+++ server/routes/tpo.js
@@ -16,25 +16,38 @@ async function tpo(req, res) {
     // 여기서 데이터베이스 작업 수행
 
     // 유저가 보낸 자연어(message)를 우선 필터링해오는 기능
-    const result = await connection.execute('SELECT STYLE_IDS FROM T_STYLE WHERE STYLE_CMT LIKE :search OR STYLE_TAG LIKE :search', { search: '%' + search + '%' }, [search, search]);
-    const style_result = await connection.execute('SELECT STYLE_URL FROM T_STYLE WHERE STYLE_CMT LIKE :search OR STYLE_TAG LIKE :search', { search: '%' + search + '%' }, [search, search]);
+    const result = await connection.execute('SELECT STYLE_IDS, STYLE_URL FROM T_STYLE WHERE STYLE_CMT LIKE :search OR STYLE_TAG LIKE :search', { search: '%' + search + '%' }, [search, search]);
     // 쿼리 결과 출력
     console.log('쿼리 결과:', result.rows[0]);
 
-    let selectedStyle = result.rows[0][0];
-    console.log(selectedStyle);
-    let selectedStyleUrl = style_result.rows[0][0];
+    if (result.rows.length > 0) {
+      // 무작위 인덱스 생성
+      const randomIndex = parseInt(Math.random() * result.rows.length);
+      const selectedRow = result.rows[randomIndex];
 
-    let ids = selectedStyle.split(',');
-    console.log(ids);
-    selectedItem = [];
-    // 각 ID에 대해 옷 정보 검색
-    for (let item of ids) {
-      let clotheResult = await connection.execute('SELECT * FROM T_CLOTHE WHERE CLOTHE_ID = :item', [item]);
-      selectedItem.push(clotheResult.rows[0]);
+      let selectedStyleIds = selectedRow[0]; // 스타일 ID
+      let selectedStyleUrl = selectedRow[1]; // 스타일 URL
+
+      console.log('선택된 스타일 ID:', selectedStyleIds);
+      console.log('선택된 스타일 URL:', selectedStyleUrl);
+      let ids = selectedStyleIds.split(',');
+      console.log(ids);
+      selectedItem = [];
+      // 각 ID에 대해 옷 정보 검색
+      for (let item of ids) {
+        let clotheResult = await connection.execute('SELECT * FROM T_CLOTHE WHERE CLOTHE_ID = :item', [item]);
+        if (clotheResult.rows.length > 0) {
+          selectedItem.push(clotheResult.rows[0]);
+        } else {
+          console.log('ID ' + item + '에 해당하는 옷 정보가 없습니다.');
+        }
+      }
+
+      res.json({ clotheInfo: selectedItem, styleInfo: selectedStyleUrl });
+      console.log('옷 정보들', selectedItem);
+    } else {
+      console.log('검색 결과가 없습니다.');
     }
-    res.json({ clotheInfo: selectedItem, styleInfo: selectedStyleUrl });
-    console.log('옷 정보들', selectedItem);
     // 작업이 끝난 후 연결을 종료
     await connection.close();
     console.log('DB 연결 종료 성공!');
