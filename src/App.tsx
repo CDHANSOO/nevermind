b0bf4b4fe160affa762815519bc269d0d21df4ed
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2023-11-24 10:44:38 +0900

    fix Pages 폴더 이름 변경

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 import NavigationBar from './components/NavigationBar';
-import Home from './Pages/Home';
-import Content from './Pages/Content';
-import Style from './Pages/Style';
-import Tpo from './Pages/Tpo';
+import Home from './pages/Home';
+import Content from './pages/Content';
+import Style from './pages/Style';
+import Tpo from './pages/Tpo';
 // import Footer from '@components/footer';
-import Detail from './Pages/Detail';
-import Sign from './Pages/Sign';
+import Detail from './pages/Detail';
+import Sign from './pages/Sign';
 import SignUp from './components/SignUp';
-import ContentDetail from 'Pages/ContentDetail';
-import MyPage from './Pages/MyPage';
+import ContentDetail from 'pages/ContentDetail';
+import MyPage from './pages/MyPage';
 
 const App: React.FC = () => {
   const [userInput, setUserInput] = useState('');
