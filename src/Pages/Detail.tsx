d29858d36d0488bd22364af75cc50bf0a527556f
ssonggit <138184841+ssonggit@users.noreply.github.com>
2023-11-25 18:57:29 +0900

    feat/main-tpo
    main/애니매이션 효과 살짝
    tpo 클릭하면 다른 이미지 보여주게

--- src/Pages/Detail.tsx
+++ src/Pages/Detail.tsx
@@ -1,124 +1,116 @@
 import React, { useState, useEffect, FC } from 'react';
 import axios from 'axios';
 
-// interface Product {
-//   name: string;
-//   storeName: string;
-//   price: string;
-// }
-
 interface DetailProps {
-    userInput: string;
-    setUserInput: (input: string) => void;
+  userInput: string;
+  setUserInput: (input: string) => void;
 }
 
 interface ServerResponse {
-    clotheInfo: Array<string[]>;
-    styleInfo: string;
+  clotheInfo: Array<string[]>;
+  styleInfo: string;
 }
 
 const Detail: FC<DetailProps> = ({ userInput, setUserInput }) => {
-    const [search, setSearch] = useState<string>('');
-    const [responseData, setResponseData] = useState<ServerResponse | null>(null);
+  const [search, setSearch] = useState<string>('');
+  const [selectedTabIndex, setSelectedTabIndex] = useState<number>(0);
+  const [selectedClotheIndex, setSelectedClotheIndex] = useState<number>(0);
+  const [selectedStyleIndex, setSelectedStyleIndex] = useState<number>(0);
+  const [responseData, setResponseData] = useState<ServerResponse | null>(null);
 
-    // 검색어를 서버로 전송하는 함수
-    const handleSearch = async (search: string) => {
-        if (search.trim() !== '') {
-            console.log('검색어:', search);
+  // 검색어를 서버로 전송하는 함수
+  const handleSearch = async (search: string) => {
+    if (search.trim() !== '') {
+      console.log('검색어:', search);
 
-            try {
-                const response = await axios.post<ServerResponse>('http://localhost:3000/tpo', { search: search });
-                const result = response.data;
-                console.log('요청 응답 : ', result);
+      try {
+        const response = await axios.post<ServerResponse>('http://localhost:3000/tpo', { search: search });
+        const result = response.data;
+        console.log('요청 응답 : ', result);
 
-                setResponseData(result); // 상태를 업데이트
-            } catch (error) {
-                console.error('요청 오류 : ', error);
-            }
-            setSearch('');
-            setUserInput(search);
-        }
-    };
+        setResponseData(result); // 상태를 업데이트
+      } catch (error) {
+        console.error('요청 오류 : ', error);
+      }
+      setSearch('');
+      setUserInput(search);
+    }
+  };
 
-    // const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    //   if (event.key === 'Enter') {
-    //     handleSearch();
-    //   }
-    // };
+  // 초기 검색 수행
+  useEffect(() => {
+    if (userInput && !responseData) {
+      handleSearch(userInput);
+    }
+  }, [userInput]);
 
-    // 초기 검색 수행
-    useEffect(() => {
-        if (userInput) {
-            handleSearch(userInput);
-        }
-    }, [userInput]);
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleSearch(search);
+      setSearch('');
+    }
+  };
 
-    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === 'Enter') {
-            handleSearch(search);
-            setSearch('');
-        }
-    };
+  const handleClick = (index: number) => {
+    setSelectedTabIndex(index);
+    setSelectedClotheIndex(index); // 선택한 스타일에 해당하는 옷 인덱스로 업데이트
+    setSelectedStyleIndex(index)
+  };
 
-    // const products: Product[] = [
-    //   { name: '블링 핫 재킷', storeName: '상점 이름', price: '17,900 원' },
-    //   { name: '블링 핫 재킷', storeName: '상점 이름', price: '17,900 원' },
-    //   { name: '블링 핫 재킷', storeName: '상점 이름', price: '17,900 원' },
-    //   { name: '새로운 상품', storeName: '새로운 상점', price: '19,900 원' },
-    //   { name: '추가 상품', storeName: '추가 상점', price: '15,900 원' },
-    //   { name: '더 많은 상품', storeName: '다른 상점', price: '21,500 원' },
-    // ];
-
-    return (
-        <div className="h-screen w-full bg-white">
-            <div className="flex items-center justify-center h-full">
-                <div className="flex flex-col">
-                    <div className="flex justify-center mb-3">
-                        <div className="w-[14.88px] h-[15px] bg-yellow-400 rounded-full" />
-                        <div className="w-[14.88px] h-[15px] ml-1 bg-amber-200 rounded-full" />
-                        <div className="w-[14.88px] h-[15px] ml-1 bg-yellow-100 rounded-full" />
-                    </div>
-                    <div className="flex justify-center relative w-full">
-                        <img className="w-[430px] max-w-[496px] max-h-[746px] rounded-xl" src={responseData?.styleInfo || 'https://via.placeholder.com/496x746'} alt="placeholder" />
-                    </div>
-                </div>
-                <div className="flex items-left flex-col md:ml-10 mt-6 md:mt-0">
-                    <div className="mb-1 text-center md:text-left">
-                        <div className="text-2xl font-extrabold font-['Pretendard']"> {userInput}</div>
-                        <div className="text-2xl font-normal font-['Pretendard']">에 대한 코디 추천은 다음과 같습니다.</div>
-                    </div>
-                    <div className="mt-2 mb-10 w-full md:w-[348px]">
-                        <div className="relative h-10">
-                            <input
-                                type="text"
-                                value={search}
-                                onChange={e => setSearch(e.target.value)}
-                                onKeyPress={e => handleKeyPress(e)}
-                                className="w-full h-full bg-zinc-100 rounded-[29px] border border-stone-300 pl-4 text-base font-extralight font-['Inter']"
-                                placeholder="다시 검색하기"
-                            />
-                        </div>
-                    </div>
-                    <div className="w-full md:w-[347px] max-h-[400px] overflow-auto">
-                        {responseData?.clotheInfo?.map((clothe, index) => (
-                            <div key={index} className="flex justify-start w-full h-32 rounded-xl border border-zinc-300 p-4 mb-2">
-                                <div className="flex items-center">
-                                    <div className="w-24 h-24 bg-zinc-300 rounded-xl mr-4">
-                                        <img src={clothe[1]} alt={clothe[2]} className="w-full h-full object-cover rounded-xl" />
-                                    </div>
-                                    <div className="text-left">
-                                        <div className="text-black text-xl font-bold font-Pretendard">{clothe[2]}</div>
-                                        <div className="text-black text-base font-normal font-Pretendard">{clothe[3]}</div>
-                                        <div className="text-black text-base font-bold font-Pretendard">{clothe[4]}</div>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="h-screen w-full bg-white">
+      <div className="flex items-center justify-center h-full">
+        <div className="flex flex-col">
+          <div className="flex justify-center mb-3">
+            {[0, 1, 2].map(index => (
+              <div
+                key={index}
+                className={`w-[14.88px] h-[15px] ml-${index === 0 ? '0' : '1'} ${index === selectedTabIndex ? 'bg-yellow-400' : index === 1 ? 'bg-amber-200' : 'bg-yellow-100'} rounded-full`}
+                onClick={() => handleClick(index)}
+              />
+            ))}
+          </div>
+          <div className="flex justify-center relative w-full">
+            <img className="w-[430px] max-w-[496px] max-h-[746px] rounded-xl" src={responseData?.styleInfo[selectedStyleIndex] || 'https://via.placeholder.com/496x746'} alt="placeholder" />
+          </div>
         </div>
-    );
+        <div className="flex items-left flex-col md:ml-10 mt-6 md:mt-0">
+          <div className="mb-1 text-center md:text-left">
+            <div className="text-2xl font-extrabold font-['Pretendard']"> {userInput}</div>
+            <div className="text-2xl font-normal font-['Pretendard']">에 대한 코디 추천은 다음과 같습니다.</div>
+          </div>
+          <div className="mt-2 mb-10 w-full md:w-[348px]">
+            <div className="relative h-10">
+              <input
+                type="text"
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+                onKeyPress={e => handleKeyPress(e)}
+                className="w-full h-full bg-zinc-100 rounded-[29px] border border-stone-300 pl-4 text-base font-extralight font-['Inter']"
+                placeholder="다시 검색하기"
+              />
+            </div>
+          </div>
+          <div className="w-full md:w-[347px] max-h-[400px] overflow-auto">
+            {responseData?.clotheInfo[selectedClotheIndex]?.map((clothe, index) => (
+              <div key={index} className="flex justify-start w-full h-32 rounded-xl border border-zinc-300 p-4 mb-2">
+                <div className="flex items-center">
+                  <div className="w-24 h-24 bg-zinc-300 rounded-xl mr-4">
+                    <img src={clothe[1]} alt={clothe[2]} className="w-full h-full object-cover rounded-xl" />
+                  </div>
+                  <div className="text-left">
+                    <div className="text-black text-xl font-bold font-Pretendard">{clothe[2]}</div>
+                    <div className="text-black text-base font-normal font-Pretendard">{clothe[3]}</div>
+                    <div className="text-black text-base font-bold font-Pretendard">{clothe[4]}</div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
--- src/components/mainPage/MainCom.tsx
+++ src/components/mainPage/MainCom.tsx
@@ -1,41 +1,49 @@
+// MainCom.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowForward } from 'react-icons/io';
+import { useInView } from 'react-intersection-observer';
+import '../../animationcss/mainCom.css';
 
-interface Props{
+interface Props {
   img: string
   link: string
 }
 
-const MainCom = ({img,link}: Props) => {
+const MainCom = ({ img, link }: Props) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
+
+  const animationClass = inView ? 'animate-fadeIn' : '';
+
   return (
-    <>
-      <div className='relative w-full h-[clac(100vh-70px)] top-[70px]'>
-        <div className='flex justify-center'>
-          <div className='pl-[100px] w-[720px] h-[720px] bg-[white] flex flex-col justify-center item-center rounded-[10px] mr-[25px]'>
-            <div className=''>
-              <span className='p-[10px] bg-[#edeafd] text-[#7453ea] rounded-[10px]'>
-                STYLE 추천 플랫폼
-              </span>
-            </div>
-            <h2 className='text-5xl font-extrabold mt-[20px]'>코디를 추천해주는<br />플랫폼이 필요하신가요?</h2>
-            <p className='mt-[10px]'>간단한 사진 및 텍스트를 통해서, 효율적인 코디를 추천받아보세요. <br /> 만족하는 차별화된 코디추천 서비스를 제공합니다.</p>
-            <Link to={link} >
-              <div className='w-[500px] h-[96px] flex items-center bg-[#fafafa] rounded-[10px]'>
-                  <div className='text-3xl font-black ml-[10px]'>NEVERMIND</div>
-                  <div className='ml-[20px] mt-[3px] bg-[#edeafd] p-[8px] rounded-[10px] hover:scale-110'>시작하기</div>
-                  <div className='ml-[200px] '>
-                    <IoIosArrowForward/>
-                  </div>
+    <div className={`relative w-full h-[clac(100vh-70px)] top-[70px] ${animationClass}`} ref={ref}>
+      <div className="flex justify-center">
+        <div className={`pl-[100px] w-[720px] h-[720px] bg-[white] flex flex-col justify-center item-center rounded-[10px] mr-[25px] ${animationClass}`}>
+          <div className={`animate-box ${animationClass}`}>
+            <span className="p-[10px] bg-[#edeafd] text-[#7453ea] rounded-[10px]">STYLE 추천 플랫폼</span>
+          </div>
+          <h2 className='text-5xl font-extrabold mt-[20px]'>코디를 추천해주는<br />플랫폼이 필요하신가요?</h2>
+          <p className={`mt-[10px] ${animationClass}`}>
+            간단한 사진 및 텍스트를 통해서, 효율적인 코디를 추천받아보세요. <br />
+            만족하는 차별화된 코디추천 서비스를 제공합니다.
+          </p>
+          <Link to={link}>
+            <div className={`w-[500px] h-[96px] flex items-center bg-[#fafafa] rounded-[10px] ${animationClass}`}>
+              <div className={`text-3xl font-black ml-[10px] ${animationClass}`}>NEVERMIND</div>
+              <div className={`ml-[20px] mt-[3px] bg-[#edeafd] p-[8px] rounded-[10px] hover:scale-110 ${animationClass}`}>시작하기</div>
+              <div className={`ml-[200px] ${animationClass}`}>
+                <IoIosArrowForward />
               </div>
-            </Link>
-          </div>
-          <div className='w-[720px] h-[720px] ml-[25px]'>
-            <img src={img} className='w-full h-full rounded-[20px]'/>
-          </div>
+            </div>
+          </Link>
+        </div>
+        <div className={`w-[720px] h-[720px] ml-[25px] ${animationClass}`}>
+          <img src={img} className={`w-full h-full rounded-[20px] ${animationClass}`} />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
