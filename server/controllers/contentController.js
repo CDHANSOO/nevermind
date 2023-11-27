173f3601638b11e9fcd7c9eb05a2675147516ee7
GitHansu <138184682+GitHansu@users.noreply.github.com>
2023-11-27 14:19:20 +0900

    기능완성
    S3에 업로드한 이미지를 fastapi로 보내서 정보를 받아오는데 성공함!

--- server/controllers/contentController.js
+++ server/controllers/contentController.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const afterUploadImage = async (req, res) => {
   if (req.file) {
-    console.log(req.file);
+    console.log('req.file 정보 :', req.file);
 
     try {
       // FastAPI 서버의 URL. 이 URL은 FastAPI 서버의 엔드포인트에 맞게 수정해야 합니다.
@@ -9,14 +9,13 @@ const afterUploadImage = async (req, res) => {
 
       // FastAPI 서버로 요청을 보냅니다. 이 예시에서는 이미지의 URL을 JSON 형태로 보냅니다.
       const response = await axios.post(fastApiUrl, {
-        params: {
-          url: req.file.location, // S3에 업로드된 이미지의 URL
-          k: 5, // 필요한 경우 변경 가능
-        },
+        url: req.file.location, // S3에 업로드된 이미지의 URL
+        k: 5, // 필요한 경우 변경 가능
       });
 
       // FastAPI 서버로부터의 응답을 프론트엔드로 전달합니다.
       res.json({ message: '파일이 성공적으로 업로드되었습니다.', url: req.file.location, fastApiResponse: response.data });
+      console.log('Fastapi 전송 데이터', response.data);
     } catch (error) {
       console.error('FastAPI 서버 요청 중 오류 발생:', error);
       res.status(500).send('서버에서 오류가 발생했습니다.');
