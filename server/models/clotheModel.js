38e3409822478bbf83d44feda1dfac180c135f36
GitHansu <138184682+GitHansu@users.noreply.github.com>
2023-11-28 17:41:41 +0900

    컨텐츠코디 메인 기능 완료
    컨텐츠 코드의 메인 기능인 딥러닝과 연결후 받아온 코디 정보를 이용해 다시 그 코디에 해당하는 상품 정보를 받아오고 이를 프론트엔드로 보내주는 기능을 구현하는데 드디어 성공하였습니다,,,

--- server/models/clotheModel.js
+++ server/models/clotheModel.js
@@ -1,12 +1,26 @@
 const dbConnector = require('../config/dbConnector');
 
-// style 찾는 코드 {작성자 : '이한수'}
+// 검색으로 style 찾는 코드 {작성자 : '이한수'}
 async function getStylesBySearch(search) {
   const connection = await dbConnector.dbConnect();
   try {
     const query = 'SELECT STYLE_IDS, STYLE_URL FROM T_STYLE WHERE STYLE_CMT LIKE :search OR STYLE_TAG LIKE :search';
     const result = await connection.execute(query, { search: '%' + search + '%' }, [search, search]);
     return result.rows;
+  } finally {
+    await connection.close();
+  }
+}
+
+async function getStylesById(id) {
+  const connection = await dbConnector.dbConnect();
+  try {
+    const query = 'SELECT STYLE_IDS, STYLE_URL FROM T_STYLE WHERE STYLE_ID = :id';
+    const result = await connection.execute(query, { id: id }, [id]);
+    console.log('getStylesById 결과:', result.rows);
+    return result.rows;
+  } catch (error) {
+    console.error('id 스타일로 찾는 거 ', error.message);
   } finally {
     await connection.close();
   }
@@ -23,4 +37,4 @@ async function getClotheById(item) {
   }
 }
 
-module.exports = { getStylesBySearch, getClotheById };
+module.exports = { getStylesBySearch, getClotheById, getStylesById };
