2e13aa77b4e797cee9fd09e1c9cca9afa6026a72
GitHansu <138184682+GitHansu@users.noreply.github.com>
2023-12-09 20:25:53 +0900

    Update oracle.js
    보안상 위험한 부분 수정

--- server/config/oracle.js
+++ server/config/oracle.js
@@ -1,6 +1,6 @@
 module.exports = {
-  user: process.env.DB_USER || 'Insa4_JSA_final_5',
-  password: process.env.DB_PASSWORD || 'aishcool0607',
-  connectString: process.env.DB_CONNECT_STRING || 'project-db-stu3.smhrd.com:1524/XE',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD ,
+  connectString: process.env.DB_CONNECT_STRING ,
   externalAuth: false,
 };
